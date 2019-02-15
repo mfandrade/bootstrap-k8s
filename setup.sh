5feedfb7a208a079bf562c0536d3a93aa55bcf5e
Marcelo F Andrade <mfandrade@gmail.com>
2019-02-15 12:40:21 -0300

    install k8s and docker via repository

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 DEBUG=true
+REDO=true
 
 APT=/usr/bin/apt-get
 TEE=/usr/bin/tee
@@ -23,34 +24,81 @@ file { '.curlrc':
   path    => '$HOME/.curlrc',
   ensure  => file,
   content => 'proxy = http://autenticador:Autent1c$50d0r@10.8.14.22:6588',
+  before  => Package['curl'],
 }
-package { 'curl':
+package { ['apt-transport-https', 'ca-certificates', 'curl', 'gnupg2', 'software-properties-common']:
   ensure  => present,
-  require => File['.curlrc'],
+  require => Exec['apt-update'],
+}
+exec { 'apt-update':
+  command   => 'apt-get update',
+  subscribe => [ File['docker.list'], File['kubernetes.list'] ],
 }
 EOF
 
 # -----------------------------------------------------------------------------
 # Install Docker
-# https://docs.docker.com/install/linux/docker-ce/debian/#install-using-the-convenience-script
+# https://docs.docker.com/install/linux/docker-ce/debian/#install-using-the-repository
 $TEE -a $FILE <<EOF >/dev/null
 package { ['docker', 'docker-engine', 'docker.io', 'containerd', 'runc']:
   ensure => absent,
-  before => Exec['get-docker'],
+  before => Package['docker-ce'],
 }
-exec { 'get-docker':
-  command => 'curl -fsSL https://get.docker.com -o get-docker.sh',
+exec { 'get-docker-key':
+  command => 'curl -fsSL https://download.docker.com/linux/debian/gpg -o docker-key',
   require => Package['curl'],
 }
-exec { 'install-docker':
-  command => 'sh get-docker.sh',
-  unless  => 'dpkg -s docker-ce-cli',
-  require => Exec['get-docker'],
+exec { 'add-docker-key':
+  command =>'apt-key add docker-key',
+  require => Exec['get-docker-key'],
+}
+file { 'docker.list':
+  path    => '/etc/apt/sources.list.d/docker.list',
+  ensure  => file,
+  content => 'deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable',
+}
+package { ['docker-ce', 'docker-ce-cli', 'containerd.io']:
+  ensure  => installed,
+  require => File['docker.list'],
 }
 EOF
 
+# -----------------------------------------------------------------------------
+# prepare to install docker-compose
+# URL=$(curl -sL -o /dev/null -w %{url_effective} https://github.com/docker/compose/releases/latest)
+# VERSION=$(echo $URL | cut -d'/' -f8)
+# curl -L "https://github.com/docker/compose/releases/download/$VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#
+# TODO: the same for docker-machine
 
-/usr/bin/puppet apply $FILE 2>&1 >/dev/null
+# -----------------------------------------------------------------------------
+# Install Kubernetes
+# https://kubernetes.io/docs/setup/independent/install-kubeadm/#installing-kubeadm-kubelet-and-kubectl
+$TEE -a $FILE <<EOF >/dev/null
+exec { 'get-kubernetes-key':
+  command => 'curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg -o kubernetes-key',
+  require => Package['curl'],
+}
+exec { 'add-kubernetes-key':
+  command => 'apt-key add kubernetes-key',
+  require => Exec['get-kubernetes-key'],
+}
+file { 'kubernetes.list':
+  path    => '/etc/apt/sources.list.d/kubernetes.list',
+  ensure  => file,
+  content => 'deb https://apt.kubernetes.io/ kubernetes-xenial main',
+}
+package { ['kubelet', 'kubeadm', 'kubectl']:
+  ensure  => held,
+  require => File['kubernetes.list'],
+}
+EOF
+
+if test $DEBUG; then
+  /usr/bin/puppet apply $FILE --verbose
+else
+  /usr/bin/puppet apply $FILE 2>&1 >/dev/null
+fi
 
 # testing
 /usr/bin/docker run hello-world | grep '^Hello'
@@ -60,9 +108,14 @@ test ! $DEBUG && /bin/systemctl enable docker
 #/usr/sbin/usermod -aG docker your-user
 
 # redo step
-test $DEBUG && $APT purge -y --autoremove docker-ce docker-ce-cli 2>&1 >/dev/null
-test $DEBUG && /bin/rm -rf /var/lib/docker
-
+if test $REDO; then
+  $APT purge -y --autoremove docker-ce docker-ce-cli containerd.io 2>&1 >/dev/null
+  /bin/rm /etc/apt/sources.list.d/docker.list
+  /bin/rm -rf /var/lib/docker
+  /usr/bin/apt-mark unhold kubelet kubeadm kubectl 2>&1 >/dev/null
+  $APT purge -y --autoremove kubelet kubeadm kubectl 2>&1 >/dev/null
+  /bin/rm /etc/apt/sources.list.d/kubernetes.list
+fi
 
 # cleanup
-test ! $DEBUG && /bin/rm -rf $DIR
+test ! $REDO && /bin/rm -rf $DIR
