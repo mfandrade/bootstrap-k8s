8976b576a69b2f22798b12de0dd614dd6e3e03aa
Marcelo F Andrade <mfandrade@gmail.com>
2019-02-15 09:23:59 -0300

    first version installing docker with script

---
+++ setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+APT=/usr/bin/apt-get
+TEE=/usr/bin/tee
+#DIR=$(/bin/mktemp -d)
+DIR=/tmp/ppp
+/bin/mkdir $DIR
+FILE="$DIR/install.pp"
+
+# Install Puppet as requirement
+if test ! -x /usr/bin/puppet; then
+    $APT update && $APT install -y puppet 2>&1 >/dev/null
+fi
+$TEE $FILE <<EOF >/dev/null
+Exec {
+  cwd  => '$DIR',
+  path => '/usr/bin:/bin',
+}
+file { '.curlrc':
+  path    => '$HOME/.curlrc',
+  ensure  => file,
+  content => 'proxy = http://autenticador:Autent1c$50d0r@10.8.14.22:6588',
+}
+package { 'curl':
+  ensure  => present,
+  require => File['.curlrc'],
+}
+EOF
+
+# -----------------------------------------------------------------------------
+# Install Docker
+# https://docs.docker.com/install/linux/docker-ce/debian/#install-using-the-convenience-script
+$TEE -a $FILE <<EOF >/dev/null
+exec { 'get-docker':
+  command => 'curl -fsSL https://get.docker.com -o get-docker.sh',
+  require => Package['curl'],
+}
+exec { 'install-docker':
+  command => 'sh get-docker.sh',
+  require => Exec['get-docker'],
+}
+EOF
+
+/usr/bin/puppet apply $FILE 2>&1 >/dev/null
+/usr/bin/docker run hello-world | grep '^Hello'
+/bin/systemctl enable docker
+
+
+$APT purge -y --autoremove docker-ce docker-ce-cli 2>&1 >/dev/null
+/bin/rm -rf /var/lib/docker
+
+#/usr/sbin/usermod -aG docker your-user
+/bin/rm -rf $DIR
