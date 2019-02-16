432c462c0e162024c998d85a8bc3b22035aa8475
Marcelo F Andrade <mfandrade@gmail.com>
2019-02-16 15:26:35 -0300

    add check_distro function

--- setup.sh
+++ setup.sh
@@ -16,10 +16,54 @@ err()
     echo "$@" >&2
 }
 
+check_distro()
+{
+    local vregexp='\([[:alpha:]]*\)'
+    if [ -f /etc/os-release ]; then
+        source /etc/os-release
+        distro="$ID"
+        version=$(echo $VERSION_ID | cut -f1 -d.)
+        codename=$(echo $VERSION | grep -o $vregexp | tr [A-Z] [a-z])
+    elif command -v lsb_release &>/dev/null; then
+        distro="$(lsb_release -si | tr [A-Z] [a-z])"
+        version="$(lsb_release -sr | cut -f1 -d.)"
+        codename=$(lsb_release -sc | grep $vregexp | tr [A-Z] [a-z])
+    else
+        err "Could not get the version of this distro."
+        exit 1
+    fi
+    echo $distro $version $codename
+}
+
 requirements()
 {
-    command -v puppet &>/dev/null || apt-get install -y puppet &>/dev/null
-    command -v curl &>/dev/null || apt-get install -y curl &>/dev/null
+    local so=$(check_distro)
+    local distro=${so[0]}
+    local version=${so[1]}
+    local codename=${so[2]}
+    case $distro in
+        redhat|centos)
+            yum install -y curl &>/dev/null
+            local pkg="puppet5-release-el-${version}.noarch.rpm"
+            local url="https://yum.puppet.com/puppet5/${pkg}"
+            curl -sL -O $url && \
+            rpm -Uvh $pkg &>/dev/null && \
+                yum install -y puppet-agent &>/dev/null
+            ;;
+
+        debian|ubuntu)
+            apt-get install -y curl &>/dev/null
+            local pkg="puppet5-release-${codename}.deb"
+            local url="https://apt.puppetlabs.com/${pkg}"
+            curl -sL -O $url && \
+                dpkg -i $pkg &>/dev/null && \
+                apt-get update &>/dev/null
+                apt-get install -y puppet-agent &>/dev/null
+        *)
+            err "Distribution not yet supported."
+            exit
+            ;;
+
 }
 
 puppet_begin()
@@ -175,4 +219,4 @@ main()
     postinstall
 }
 
-main
+requirements
