18b77f04161622939a118e24342f585a59fe9dea
Marcelo F Andrade <mfandrade@gmail.com>
2019-02-22 10:27:08 -0300

    error message

--- install.sh
+++ install.sh
@@ -79,7 +79,8 @@ node 'vm-k8s-master.trt8.net', 'vm-k8s-nodes.trt8.net' {
 }
 EOF
   puppet apply $dir/site.pp --modulepath=modules/ --verbose
-  [[ -x /usr/bin/docker ]] && docker run hello-world | grep '^Hello' || err ':( Docker could not be installed.  Check any previous error messages.'
+  [[ -x /usr/bin/docker ]] && docker run hello-world | grep '^Hello' \
+    || err 'Docker could not be installed :('
 }
 
 main
